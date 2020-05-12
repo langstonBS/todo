99c3a4e8f8b57520df1f052fc4f961b638de0fc9
langstonBS <langston.beckwithstanley@gmail.com>
2020-05-12 13:25:26 -0700

    first commit

---
+++ data/load-seed-data.js
@@ -0,0 +1,46 @@
+const client = require('../lib/client');
+// import our seed data:
+const todoList = require('./todo.js');
+const usersData = require('./users.js');
+
+run();
+
+async function run() {
+
+  try {
+    await client.connect();
+
+    const users = await Promise.all(
+      usersData.map(user => {
+        return client.query(`
+                      INSERT INTO users (email, hash)
+                      VALUES ($1, $2)
+                      RETURNING *;
+                  `,
+        [user.email, user.hash]);
+      })
+    );
+      
+    const user = users[0].rows[0];
+
+    await Promise.all(
+      todoList.map(todo => {
+        return client.query(`
+                    INSERT INTO todo (task_name, completed, user_id)
+                    VALUES ($1, $2, $3);
+                `,
+        [todo.name, todo.completed, user.id]);
+      })
+    );
+    
+
+    console.log('seed data load complete');
+  }
+  catch(err) {
+    console.log(err);
+  }
+  finally {
+    client.end();
+  }
+    
+}
---
+++ data/todo.js
@@ -0,0 +1,11 @@
+module.exports = [
+  {
+    task_name: 'bessie'
+  },
+  {
+    task_name: 'jumpy'
+  },
+  {
+    task_name: 'spot'
+  }
+];

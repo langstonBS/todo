9774abf5fc76a936b49a86331e5811e6641ecb94
langstonBS <langston.beckwithstanley@gmail.com>
2020-05-12 16:36:18 -0700

    testing out sql

--- data/create-tables.js
+++ data/create-tables.js
@@ -19,7 +19,7 @@ async function run() {
                 CREATE TABLE todo (
                     id SERIAL PRIMARY KEY NOT NULL,
                     task_name VARCHAR(512) NOT NULL,
-                    completed BOOL DEFAULT FALSE,
+                    completed BOOLEAN DEFAULT FALSE,
                     user_id INTEGER NOT NULL REFERENCES users(id)
             );
         `);
